ea100fc16be399534763bcc45cd650a8a21d032d
Lumix1337 <olzagula@gmail.com>
2026-05-22 22:30:01 +0500

    feat: L15 data binding on MainPage

---
+++ SchoolApp/ViewModels/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolApp.ViewModels
+{
+    public class Person
+    {
+        public string Name { get; set; } = "Ansar";
+        public string City { get; set; } = "Atyrau";
+    }
+}
